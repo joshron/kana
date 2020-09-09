d5a7ee79a115195d50062e8c69d3e056f074b0f1
Josh Palmer <josh.palmer@radlink.com.au>
2020-09-09 09:45:05 +0800

    first commit

---
+++ exportAll.py
@@ -0,0 +1,22 @@
+import os
+
+os.system('python exporter.py --format pdf --config rangecontrol.yaml')
+print('Range Control checked')
+
+os.system('python exporter.py --format pdf --config sitesurvey.yaml')
+print('Site Survey checked')
+
+os.system('python exporter.py --format pdf --config imark.yaml')
+print('iMark checked')
+
+os.system('python exporter.py --format pdf --config unplanned.yaml')
+print('Unplanned checked')
+
+os.system('python exporter.py --format pdf --config take5.yaml')
+print('Take5 checked')
+
+os.system('python exporter.py --format pdf --config vehicle.yaml')
+print('Vehicle Prestart checked')
+
+os.system('python exporter.py --format pdf --config journey.yaml')
+print('Journey Management checked')

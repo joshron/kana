9a95244643397d9ec4021bbc4e124ae2377c6393
Josh Palmer <josh-palmer@live.com>
2020-12-16 10:15:25 +0800

    Editing js script

--- app.js
+++ app.js
@@ -9,9 +9,16 @@ function makeRequest() {
     }
 
     httpRequest.onreadystatechange = alertContents;
-    httpRequest.open("GET", "")
+    httpRequest.open("GET", "https://raw.githubusercontent.com/joshron/kana/master/kanas.json");
+    httpRequest.send();
 }
 
 function alertContents() {
-
+    if (httpRequest.readyState === XMLHttpRequest.DONE) {
+        if (httpRequest.status === 200) {
+            alert(httpRequest.hiragana.a.ka.romaji);
+        } else {
+            alert("There was a problem with the request");
+        }
+    }
 }

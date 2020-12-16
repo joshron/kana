03ed85ee3d81238322c2efc6d5a0b552e9820fe2
Josh Palmer <josh-palmer@live.com>
2020-12-16 11:12:12 +0800

    Fixing typos in json file

--- app.js
+++ app.js
@@ -1,24 +1,20 @@
-let httpRequest;
+const requestUrl = "https://raw.githubusercontent.com/joshron/kana/master/kanas.json";
+const request = new XMLHttpRequest();
+const activeArea = document.getElementById("main");
+let json;
 
-function makeRequest() {
-    httpRequest = new XMLHttpRequest();
-
-    if (!httpRequest) {
-        alert("There was an error with the XMLHttp request")
-        return false;
-    }
-
-    httpRequest.onreadystatechange = alertContents;
-    httpRequest.open("GET", "https://raw.githubusercontent.com/joshron/kana/master/kanas.json");
-    httpRequest.send();
+request.open("GET", requestUrl);
+request.responseType = "json";
+request.send();
+request.onload = () => {
+    console.log("json request success");
+    json = request.response;
+    testFunc();
 }
 
-function alertContents() {
-    if (httpRequest.readyState === XMLHttpRequest.DONE) {
-        if (httpRequest.status === 200) {
-            alert(httpRequest.hiragana.a.ka.romaji);
-        } else {
-            alert("There was a problem with the request");
-        }
-    }
+function testFunc() {
+    const div = document.createElement("div");
+    const text = document.createTextNode(json.hiragana.sa.sa.romaji);
+    div.append(text);
+    activeArea.append(div);
 }

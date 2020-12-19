394b4f30a6cfd5049feb231b8a7d2ad3c2d9b1b8
Josh Palmer <josh-palmer@live.com>
2020-12-19 19:38:51 +0800

    Added options button to index and associated js scripts

--- app.js
+++ app.js
@@ -2,10 +2,18 @@ const requestUrl = "https://raw.githubusercontent.com/joshron/kana/master/kanas.
 const request = new XMLHttpRequest();
 const activeArea = document.getElementById("main");
 const hiddenDiv = document.getElementById("hidden-div");
+const studyContainer = document.getElementById("study-container");
+const studyCardsContainer = document.getElementById("study-cards-container");
 
+//All buttons
+const buttonContainer = document.getElementById("button-container");
 const studyButton = document.getElementById("study-button");
 const practiceButton = document.getElementById("practice-button");
 const chartButton = document.getElementById("chart-button");
+const closeButton = document.getElementById("close-button");
+const baseHiraganaButton = document.getElementById("base-hiragana-button");
+const modifiedHiraganaButton = document.getElementById("modified-hiragana-button");
+const allHiraganaButton = document.getElementById("all-hiragana-button");
 
 const hiraganaTable = document.getElementById("hiragana-table");
 let json;
@@ -19,6 +27,38 @@ request.onload = () => {
 }
 
 chartButton.addEventListener("click", () => {
-    activeArea.innerHTML = "";
-    activeArea.append(hiraganaTable);    
+    hiddenDiv.append(buttonContainer);
+    activeArea.append(hiraganaTable);
+    activeArea.append(closeButton);    
 });
+
+closeButton.addEventListener("click", () => {
+    hiddenDiv.append(closeButton);
+    activeArea.append(buttonContainer);
+    if (activeArea.firstElementChild === hiraganaTable) {
+        hiddenDiv.append(hiraganaTable);
+    } else if (activeArea.firstElementChild === studyContainer) {
+        hiddenDiv.append(studyContainer);
+    }
+});
+
+studyButton.addEventListener("click", () => {
+    hiddenDiv.append(buttonContainer);
+    activeArea.append(studyContainer);
+    activeArea.append(closeButton);
+})
+
+baseHiraganaButton.addEventListener("click", () => {studyCards(baseHiraganaArray)});
+modifiedHiraganaButton.addEventListener("click", () => {studyCards(modifiedHiraganaArray)});
+allHiraganaButton.addEventListener("click", () => {studyCards(allHiraganaArray)});
+
+
+const baseHiraganaArray = ["a", "ka", "sa", "ta", "na", "ha", "ma", "ya", "ra", "wa", "n"];
+const modifiedHiraganaArray = ["ga", "za", "da", "ba", "pa"];
+const allHiraganaArray = baseHiraganaArray.concat(modifiedHiraganaArray);
+function studyCards(someArray) {
+    for (i = 0; i < someArray.length; i++) {
+        const rowArray = Object.keys(json.hiragana[someArray[i]]);
+        console.log(rowArray);
+    }    
+}
